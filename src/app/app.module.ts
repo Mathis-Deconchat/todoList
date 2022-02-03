53893e5c1f99ff54d503973cbe388a97b41c32fc
mathis.Deconchat@gmail.com <mathis.deconchat@gmail.com>
2022-02-03 15:37:56 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIpfkclpn5xb0DdDURmyA32V3qT5IS-_E",
+  authDomain: "todoionic-bd2e2.firebaseapp.com",
+  databaseURL: "https://todoionic-bd2e2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "todoionic-bd2e2",
+  storageBucket: "todoionic-bd2e2.appspot.com",
+  messagingSenderId: "325246154438",
+  appId: "1:325246154438:web:9c75436f39c4189f2d09c7",
+  measurementId: "G-WFF3N0D1JY"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public afDB: AngularFireDatabase) {
+    this.currentDate = moment().format("Do MM YYYY");
+  }
+
+  currentDate: string;
+  myTask = '';
+  addTask: boolean;
+
+  addTaskToFirebase() {
+    console.log("clicked");
+    this.afDB.list("/Tasks").push({
+      text: this.myTask,
+      date: new Date().toISOString(),
+      checked: false
+    })
+    this.showForm();
+  }
+
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
+
+
 
 }
